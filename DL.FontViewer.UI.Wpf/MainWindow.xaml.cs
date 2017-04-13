15d94fd959884164d2c225adfedc212f1dac8614
Jeremy <jeremy@theknights.me>
2017-04-13 09:22:20 -0500

    initial commit into github

---
+++ DL.FontViewer.UI.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.TextFormatting;
+
+namespace DL.FontViewer.UI.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        internal FontViewerViewModel ViewModel
+        {
+            get { return this.DataContext as FontViewerViewModel;  }
+        }
+
+        public MainWindow()
+        {
+            this.DataContext = new FontViewerViewModel();
+            InitializeComponent();
+        }
+
+        private void BoldButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.ViewModel.SelectedFontWeight = FontWeights.Bold;
+        }
+
+        private void ItalicsButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.ViewModel.SelectedFontStyle = FontStyles.Italic;
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.ViewModel.SelectedFontStyle = FontStyles.Normal;
+            this.ViewModel.SelectedFontWeight = FontWeights.Normal;
+        }
+
+        private void SearchTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter || e.Key == Key.Return)
+            {
+                string filter = (sender as TextBox).Text;
+                this.ViewModel.FilterFonts(filter);
+            }
+        }
+    }
+}
