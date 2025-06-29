c3a051fdefee7c88fffd55c2620a3984df83da5a
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2025-06-29 12:56:28 -0500

    converted to blazor web view

--- FontViewer/MainWindow.xaml.cs
+++ FontViewer/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FontViewer;
 
@@ -9,39 +8,11 @@ namespace FontViewer;
 /// </summary>
 public partial class MainWindow : Window
 {
-    internal FontViewerViewModel ViewModel
-    {
-        get { return DataContext as FontViewerViewModel; }
-    }
-
     public MainWindow()
     {
-        DataContext = new FontViewerViewModel();
         InitializeComponent();
-    }
-
-    private void BoldButton_Click(object sender, RoutedEventArgs e)
-    {
-        ViewModel.SelectedFontWeight = FontWeights.Bold;
-    }
-
-    private void ItalicsButton_Click(object sender, RoutedEventArgs e)
-    {
-        ViewModel.SelectedFontStyle = FontStyles.Italic;
-    }
-
-    private void ClearButton_Click(object sender, RoutedEventArgs e)
-    {
-        ViewModel.SelectedFontStyle = FontStyles.Normal;
-        ViewModel.SelectedFontWeight = FontWeights.Normal;
-    }
-
-    private void SearchTextBox_KeyDown(object sender, KeyEventArgs e)
-    {
-        if (e.Key == Key.Enter || e.Key == Key.Return)
-        {
-            string filter = (sender as TextBox).Text;
-            ViewModel.FilterFonts(filter);
-        }
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddWpfBlazorWebView();
+        Resources.Add("services", serviceCollection.BuildServiceProvider());
     }
 }
