e01cb86feaacff20486b6d19867ed73a7fbc1456
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2025-06-29 14:02:16 -0500

    blazor web view version (#3)
    * added readme to solution
    * converted to blazor web view
    * added preview logic specifically for blazor

--- FontViewer/MainWindow.xaml.cs
+++ FontViewer/MainWindow.xaml.cs
@@ -1,48 +1,18 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace FontViewer
+namespace FontViewer;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        internal FontViewerViewModel ViewModel
-        {
-            get { return this.DataContext as FontViewerViewModel; }
-        }
-
-        public MainWindow()
-        {
-            this.DataContext = new FontViewerViewModel();
-            InitializeComponent();
-        }
-
-        private void BoldButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.ViewModel.SelectedFontWeight = FontWeights.Bold;
-        }
-
-        private void ItalicsButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.ViewModel.SelectedFontStyle = FontStyles.Italic;
-        }
-
-        private void ClearButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.ViewModel.SelectedFontStyle = FontStyles.Normal;
-            this.ViewModel.SelectedFontWeight = FontWeights.Normal;
-        }
-
-        private void SearchTextBox_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter || e.Key == Key.Return)
-            {
-                string filter = (sender as TextBox).Text;
-                this.ViewModel.FilterFonts(filter);
-            }
-        }
+        InitializeComponent();
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddWpfBlazorWebView();
+        Resources.Add("services", serviceCollection.BuildServiceProvider());
     }
 }
